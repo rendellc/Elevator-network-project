075bc194e23a7b4c25ed867109d200920cfbb440
Bendik <bendia@stud.ntnu.no>
2018-02-19 17:11:08 +0100

    Testing peers

--- src/network/peers/peers.go
+++ src/network/peers/peers.go
@@ -14,10 +14,11 @@ type PeerUpdate struct {
 	Lost  []string
 }
 
+
 const interval = 15 * time.Millisecond
 const timeout = 50 * time.Millisecond
 
-func Transmitter(port int, id string, transmitEnable <-chan bool) {
+func Transmitter(port int, id string, transmitEnable <-chan bool, statusCh <-chan []byte) {
 
 	conn := conn.DialBroadcastUDP(port)
 	addr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("255.255.255.255:%d", port))
